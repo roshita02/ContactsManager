c256a911d341237833f0815df5966b0065281abf
roshita02 <roshitashakya07@gmail.com>
2019-07-27 11:00:19 +0545

    Nested form working now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-require("jquery")
-require("jquery_ujs")
+require('jquery')
+//require('jquery_ujs')
 
 //= require jquery
 //= require jquery_ujs
